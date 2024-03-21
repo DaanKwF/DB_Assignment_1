d131bda619b1848f64085416fbcf0ebe4b671598
DaanKwF <daan@kashmirworldfoundation.org>
2024-03-21 10:19:36 +0100

    Data Entry Finished

--- initialize_database.sql
+++ initialize_database.sql
@@ -1,14 +1,23 @@
 -- Drop table statements
 DROP TABLE IF EXISTS Museumzaal;
+DROP TABLE IF EXISTS Tentoonstelling;
 DROP TABLE IF EXISTS Item;
 DROP TABLE IF EXISTS Onderzoeksgroep;
 DROP TABLE IF EXISTS Onderzoeker;
+DROP TABLE IF EXISTS Gast;
+DROP TABLE IF EXISTS Loondienst;
 
 -- Entities
 CREATE TABLE Museumzaal (
     zaal_id INTEGER,
     naam VARCHAR,
     PRIMARY KEY(zaal_id)
+);
+
+CREATE TABLE Tentoonstelling (
+    tentoonstelling_id INTEGER,
+    naam VARCHAR,
+    PRIMARY KEY (tentoonstelling_id)
 );
 
 CREATE TABLE Item (
@@ -26,6 +35,7 @@ CREATE TABLE Onderzoeksgroep (
 CREATE TABLE Onderzoeker (
     onderzoeker_id INTEGER,
     naam VARCHAR,
+    functie VARCHAR,
     PRIMARY KEY(onderzoeker_id)
 );
 
@@ -40,18 +50,12 @@ CREATE TABLE Gast (
 
 CREATE TABLE Loondienst (
     onderzoeker_id INTEGER,
-    salaris REAL,
-    start_datum VARCHAR,
     contract VARCHAR,
+    salaris REAL,
+    start_datum DATE,
     PRIMARY KEY (onderzoek_id)
     FOREIGN KEY (onderzoeker_id) REFERENCES Onderzoeker
     ON DELETE CASCADE
-);
-
-CREATE TABLE Tentoonstelling (
-    tentoonstelling_id INTEGER,
-    naam VARCHAR,
-    PRIMARY KEY (tentoonstelling_id)
 );
 
 -- Data
@@ -60,6 +64,12 @@ INSERT INTO Museumzaal VALUES (2, 'Evolutie');
 INSERT INTO Museumzaal VALUES (3, 'Dinotijd');
 INSERT INTO Museumzaal VALUES (4, 'Dood');
 INSERT INTO Museumzaal VALUES (5, 'LiveScience');
+
+INSERT INTO Tentoonstelling VALUES (1, 'Prepareren van Dieren en Planten');
+INSERT INTO Tentoonstelling VALUES (2, 'Naturalis Nu!');
+INSERT INTO Tentoonstelling VALUES (3, 'Superzintuigen');
+INSERT INTO Tentoonstelling VALUES (4, 'Gif');
+INSERT INTO Tentoonstelling VALUES (5, 'T-Rex in Town');
 
 INSERT INTO Item VALUES ('00067A', 'T-Rex');
 INSERT INTO Item VALUES ('02710C', 'Haaietand');
@@ -73,17 +83,20 @@ INSERT INTO Onderzoeksgroep VALUES (3, 'Tropical Botany');
 INSERT INTO Onderzoeksgroep VALUES (4, 'Functional Traits');
 INSERT INTO Onderzoeksgroep VALUES (5, 'Evolutie, Ontwikkeling en Ecologie van Gewervelden');
 
-INSERT INTO Onderzoeker VALUES (1);
-INSERT INTO Onderzoeker VALUES (2);
-INSERT INTO Onderzoeker VALUES (3);
-INSERT INTO Onderzoeker VALUES (4, '');
-INSERT INTO Onderzoeker VALUES (5, 'Ton de Winter', 'gast');
+INSERT INTO Onderzoeker VALUES (1, 'Roxali Bijmoer', 'Collectiebeheer Botanie');
+INSERT INTO Loondienst VALUES (1, 'voltijd', 40000, 2018-03-01)
 
-INSERT INTO Tentoonstelling VALUES (1, 'Prepareren van dieren en planten');
-INSERT INTO Tentoonstelling VALUES (2, 'Naturalis Nu!');
-INSERT INTO Tentoonstelling VALUES (3, 'Superzintuigen');
-INSERT INTO Tentoonstelling VALUES (4, 'Gif');
-INSERT INTO Tentoonstelling VALUES (5, 'T-Rex in town');
+INSERT INTO Onderzoeker VALUES (2, 'Matyas Bittenbinder', 'PhD Candidate');
+INSERT INTO Loondienst VALUES (2, 'deeltijd', 25000, 2023-09-01)
+
+INSERT INTO Onderzoeker VALUES (3, 'Herman de Jong', 'Senior Onderzoeker');
+INSERT INTO Loondienst VALUES (3, 'voltijd', 75000, 1980-01-01)
+
+INSERT INTO Onderzoeker VALUES (4, 'Pim Arntzen', 'Gastonderzoeker');
+INSERT INTO Gast Values(4, 'Google', 'voltijd');
+
+INSERT INTO Onderzoeker VALUES (5, 'Ton de Winter', 'Gastonderzoeker');
+INSERT INTO Gast Values(5, 'University of Costa Rica', 'deeltijd');
 
 INSERT INTO VALUES ();
 INSERT INTO VALUES ();
